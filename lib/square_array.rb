73d564470eb3738b6603fbd54b869f9f12829ec9
Joseph Patterson <thinkitmakeituseit@gmail.com>
2020-02-13 00:25:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,10 @@
 def square_array(array)
  new_array = []
 i = 0
-integer = array[i]
+
 while (i < array.length) do
-  
-  
-  new_array << integer ** 2
-
-
+integer = array[i]  
+new_array << integer ** 2
+end
+new_array
 end
